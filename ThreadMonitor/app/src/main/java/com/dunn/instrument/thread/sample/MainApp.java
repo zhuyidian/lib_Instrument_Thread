1ca6eb72ad61f66a062cbaf141dabfbc88965834
zhuyidian <zhuyidian@coocaa.com>
2022-04-19 17:20:17 +0800

    增加threadhook
    Change-Id: I566483999fc8310618814f6d2c273c509bd6bb95

---
+++ ThreadMonitor/app/src/main/java/com/dunn/instrument/thread/sample/MainApp.java
@@ -0,0 +1,24 @@
+package com.dunn.instrument.thread.sample;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * @ClassName: MainApp
+ * @Author: ZhuYiDian
+ * @CreateDate: 2022/4/19 16:45
+ * @Description:
+ */
+public class MainApp extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
